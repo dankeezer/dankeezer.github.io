52605177913c7752a7f792c789735cffb477b14d
dankeezer <dankeezer@gmail.com>
2021-12-17 20:05:22 -0500

    Update counter js with refactor

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,27 +1,27 @@
 window.onload = visitor();
 
 function visitor() {
-  preloadImages([
-      "../images/counter-0.gif",
-      "../images/counter-1.gif",
-      "../images/counter-2.gif",
-      "../images/counter-3.gif",
-      "../images/counter-4.gif",
-      "../images/counter-5.gif",
-      "../images/counter-6.gif",
-      "../images/counter-7.gif",
-      "../images/counter-8.gif",
-      "../images/counter-9.gif",
-  ]);
+  var counterDigits = []
+  for(var i = 0; i < 10; i++){
+    counterDigits.concat(`../images/counter-${i}.gif`)
+  }
+  preloadImages([counterDigits]);
 
   var visitorNumberArray = Math.floor(Math.random() * 999999).toString().split("");
-  visitorNumberArray.forEach(updateCounterDigits)
+  var zeros = (visitorNumberArray.length - 6) * -1
+
+  for(var i = 0; i < zeros; i++){
+    visitorNumberArray = ["0"].concat(visitorNumberArray);
+  }
+
+  visitorNumberArray.forEach(updateCounterDigits);
 }
 
 function preloadImages(array) {
   if (!preloadImages.list) {
     preloadImages.list = [];
   }
+
   var list = preloadImages.list;
   for (var i = 0; i < array.length; i++) {
     var img = new Image();
